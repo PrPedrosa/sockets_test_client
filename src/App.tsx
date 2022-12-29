213706f2d42887220aebd1c72653b4e9d650dca1
PrPedrosa <prpedrosa95@gmail.com>
2022-12-29 17:48:10 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {useState, useEffect} from 'react';
+
+
+type Message = {
+  clientId: string,
+  content: string
+}
 
 function App() {
+
+  const [ws, setWs] = useState<any>({})
+  const [clientId, setClientId] = useState("")
+  const [onlineNum, setOnlineNum] = useState(0)
+  const [message, setMessage] = useState("")
+  const [messages, setMessages] = useState<Message[]>([])
+  const handleMessage = (e : React.ChangeEvent<HTMLInputElement>) => setMessage(e.target.value);
+  
+  const openConnection = () => {
+    const protocol = window.location.protocol.includes('https') ? 'wss': 'ws'
+    const connection = new WebSocket(`${protocol}://localhost:9090`)
+    setWs(connection)    
+  }
+  ws.onopen = () => console.log("openeeeed")
+  ws.onclose = () => {
+    console.log("closeeee")
+    setOnlineNum(0)
+  }
+  ws.onmessage = (msg:any) => {
+    const response = JSON.parse(msg.data);
+    console.log(response);
+
+    if(response.method === "connect"){
+      setClientId(response.clientId)
+    }
+
+    if(response.method === "details"){
+      setOnlineNum(response.onlineNum)
+    }
+
+    if(response.method === "sendMessages" || response.method === "connect"){
+      setMessages(response.content)
+    }
+  }
+
+  const handleSubmit = () => {
+    const payLoad = {
+      "method": "newMessage",
+      "clientId": clientId,
+      "content": message
+    }
+    ws.send(JSON.stringify(payLoad))
+    setMessage("")
+  }
+
+  const deleteMessage = () => {
+    const payLoad = {
+      "method": "deleteMessage",
+      "clientId": clientId,
+      "content": message
+    }
+    ws.send(JSON.stringify(payLoad))
+  }
+  
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {messages && messages.map(msg => {
+        return (
+          <div>
+          <span>{msg.content}</span>
+          {msg.clientId === clientId && <button onClick={deleteMessage}>delete</button>}
+          </div>
+        )
+      })}
+      <input type="text" name="messages" id="messages" value={message} onChange={handleMessage}/>
+      <button onClick={handleSubmit}>Send Message</button>
+      <button onClick={() => ws.close()}>close connection</button>
+      <button onClick={openConnection}>open connection</button>
+      {onlineNum ? <p>Online: {onlineNum}</p> : null}
     </div>
   );
 }
